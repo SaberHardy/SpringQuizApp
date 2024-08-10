5cdec863dbb68cf2acbfa4c023409652236464e9
SaberHardy <samirsamado5@gmail.com>
2024-08-10 14:31:06 -0700

    create Quiz model

--- src/main/java/com/quizapp/quizapp/models/Quiz.java
+++ src/main/java/com/quizapp/quizapp/models/Quiz.java
@@ -1,4 +1,20 @@
 package com.quizapp.quizapp.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String Title;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> question;
 }
