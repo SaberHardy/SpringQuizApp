e8f3598e72d8d636e6cd0c7a305407c65b99f8b9
SaberHardy <samirsamado5@gmail.com>
2024-08-10 16:00:04 -0700

    correct typo for QuizAttemptsRepository

--- src/main/java/com/quizapp/quizapp/repositories/QuizAttemptsRepository.java
+++ src/main/java/com/quizapp/quizapp/repositories/QuizAttemptsRepository.java
@@ -1,7 +1,8 @@
 package com.quizapp.quizapp.repositories;
 
+import com.quizapp.quizapp.models.QuizAttempts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuizAttemptsRepository extends JpaRepository<QuizRepository, Long> {
+public interface QuizAttemptsRepository extends JpaRepository<QuizAttempts, Long> {
 
 }
