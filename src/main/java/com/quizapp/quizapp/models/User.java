e0296433d1006828c9e2e9c5338ffd132f5de314
SaberHardy <samirsamado5@gmail.com>
2024-08-10 15:01:03 -0700

    create user model

---
+++ src/main/java/com/quizapp/quizapp/models/User.java
@@ -0,0 +1,23 @@
+package com.quizapp.quizapp.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<QuizAttempts> quizAttempts;
+}
