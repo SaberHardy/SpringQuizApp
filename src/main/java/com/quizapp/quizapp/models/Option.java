051877192c22f840ea5209b71b4a8a5a1694f2b0
SaberHardy <samirsamado5@gmail.com>
2024-08-10 14:34:16 -0700

    create Option model

---
+++ src/main/java/com/quizapp/quizapp/models/Option.java
@@ -0,0 +1,23 @@
+package com.quizapp.quizapp.models;
+
+import jakarta.persistence.*;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String text;
+    private Boolean correct;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+}
